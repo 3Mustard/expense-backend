d3b7735f03d8590fcff492f8359f4e2e25c23d70
3Mustard <m23.cullen@gmail.com>
2020-02-06 16:09:04 -0500

    add crud to AccountsController. add set_account and account_params under private

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::AccountsController < ApplicationController
+    before_action :set_account, only: [:show,:destroy]
+
+    def index 
+        accounts = Account.all
+
+        render json: accounts
+    end 
+
+    def create
+        account = Account.new(account_params)
+
+        if account.save
+            render json: account
+        else 
+            render json: {error: 'Error creating account'}
+        end 
+    end 
+
+    def show
+        render json: @account
+    end 
+
+    def destroy
+        @account.destroy 
+    end 
+
+    private
+
+    def set_account
+        @account = Account.find_by(id: params[:id])
+    end 
+
+    def account_params
+        params.require(:account).permit(:name, :balance)     
+    end 
+
 end
